2673bfbf0d452184e44492c1b27cb5427fd734a4
Tai Tran <tai.tran@mgisolutions.com>
2025-07-25 10:19:33 +0700

    initital todo app

---
+++ TodoApp/src/TodoApp.EntityFrameworkCore/EntityFrameworkCore/TodoAppDbContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace TodoApp.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class TodoAppDbContextFactory : IDesignTimeDbContextFactory<TodoAppDbContext>
+{
+    public TodoAppDbContext CreateDbContext(string[] args)
+    {
+        // https://www.npgsql.org/efcore/release-notes/6.0.html#opting-out-of-the-new-timestamp-mapping-logic
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        
+        var configuration = BuildConfiguration();
+        
+        TodoAppEfCoreEntityExtensionMappings.Configure();
+
+        var builder = new DbContextOptionsBuilder<TodoAppDbContext>()
+            .UseNpgsql(configuration.GetConnectionString("Default"));
+        
+        return new TodoAppDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../TodoApp.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
